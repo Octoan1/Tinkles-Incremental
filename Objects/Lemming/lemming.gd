fd4b001ffe463303f07883cd834e570e298b315f
Octoan1 <okloepping@gmail.com>
2026-07-11 22:42:24 -0500

    working on porting to component architecture
    implement health, hitbox, and hurtbox componets

--- Objects/Lemming/lemming.gd
+++ Objects/Lemming/lemming.gd
@@ -3,13 +3,13 @@ extends CharacterBody2D
 
 
 const SPEED = 150.0
-@export var health: float = 5.0
+#@export var health: float = 5.0
 @export var death_value: float = 3.0
 @export var damage_value: float = 1.0
 @export var life_span: float = 30.0
 var traits: Array[Trait]
 
-@onready var invuln_timer: Timer = $invuln_timer
+#@onready var invuln_timer: Timer = $invuln_timer
 @onready var lifespan_timer: Timer = $Lifespan
 @onready var lemming_cam: Camera2D = $LemmingCam
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -19,12 +19,14 @@ var click_ready = true
 var some_cam_enabled = false
 var jumping: bool = false
 
+@onready var health_component: HealthComponent = $HealthComponent
 
-var invuln_duration: float = 0.5
-var invulnerable: bool = false
+
+#var invuln_duration: float = 0.5
+#var invulnerable: bool = false
 
 func _ready() -> void:
-	invuln_timer.wait_time = invuln_duration
+	#invuln_timer.wait_time = invuln_duration
 	lifespan_timer.wait_time = life_span
 	lifespan_timer.start()
 	self.velocity.x = SPEED
@@ -44,39 +46,39 @@ func assign_traits() -> void:
 	for t in traits:
 		self.call(t.effect_name)
 
-func take_damage(damage_amount) -> void:
-	if not invulnerable:
-		
-		
-		invulnerable = true
-		invuln_timer.start()
-		
-		# calculate health_lost
-		var health_lost = health - damage_amount
-		if health_lost <= 0:
-			health_lost = health
-		else:
-			health_lost = health - (health - damage_amount)
-		
-		var total_goo = health_lost * damage_value
-		health -= damage_amount
-		
-		GameManager.modify_goo(total_goo)
-		
-		if health <= 0:
-			die()
-		else:
-			var pm: Node2D = get_tree().root.get_node("Main").get_node("ParticleManager")
-			pm.call_deferred("spawn_damage_particles", self.global_position, damage_amount)
+#func take_damage(damage_amount) -> void:
+	#if not invulnerable:
+		#
+		#
+		#invulnerable = true
+		#invuln_timer.start()
+		#
+		## calculate health_lost
+		#var health_lost = health - damage_amount
+		#if health_lost <= 0:
+			#health_lost = health
+		#else:
+			#health_lost = health - (health - damage_amount)
+		#
+		#var total_goo = health_lost * damage_value
+		#health -= damage_amount
+		#
+		#GameManager.modify_goo(total_goo)
+		#
+		#if health <= 0:
+			#die()
+		#else:
+			#var pm: Node2D = get_tree().root.get_node("Main").get_node("ParticleManager")
+			#pm.call_deferred("spawn_damage_particles", self.global_position, damage_amount)
 
-func die() -> void:
-	lemming_cam.enabled = false
-	GameManager.modify_goo(death_value)
-	
-	# testing
-	get_tree().root.get_node("Main").get_node("ParticleManager").spawn_particles(self.global_position)
-	
-	self.queue_free()
+#func die() -> void:
+	#lemming_cam.enabled = false
+	#GameManager.modify_goo(death_value)
+	#
+	## testing
+	#get_tree().root.get_node("Main").get_node("ParticleManager").spawn_particles(self.global_position)
+	#
+	#self.queue_free()
 
 func _physics_process(delta: float) -> void:
 	if jumping: 
@@ -123,11 +125,12 @@ func _on_ray_cast_2d_stopped_looking() -> void:
 	jumping = true
 	
 
-func _on_invuln_timer_timeout() -> void:
-	invulnerable = false
+#func _on_invuln_timer_timeout() -> void:
+	#invulnerable = false
 
 func _on_lifespan_timeout() -> void:
-	die()
+	#die()
+	health_component.kill()
 
 func _on_click_timer_timeout() -> void:
 	click_ready = true
@@ -136,4 +139,27 @@ func _on_click_timer_timeout() -> void:
 ### TRAITS HERE ###
 func _fed() -> void:
 	global_scale *= 1.5
-	health *= 2
+	#health *= 2
+	#health_component
+
+
+func _on_health_component_died() -> void:
+	lemming_cam.enabled = false
+	GameManager.modify_goo(death_value)
+	
+	# testing
+	get_tree().root.get_node("Main").get_node("ParticleManager").spawn_particles(self.global_position)
+	
+	self.queue_free()
+
+
+func _on_health_component_damaged(attack: Attack) -> void:
+	var damage_amount = attack.attack_damage
+
+	var total_goo = damage_amount * damage_value
+	
+	GameManager.modify_goo(total_goo)
+	
+	var pm: Node2D = get_tree().root.get_node("Main").get_node("ParticleManager")
+	pm.call_deferred("spawn_damage_particles", self.global_position, damage_amount)
+	
