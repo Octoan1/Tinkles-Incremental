3c526c7709ef2c2cd5035f7a84e945cafa8105ff
Octoan1 <okloepping@gmail.com>
2026-07-15 19:00:26 -0500

    cleaned up warnings

--- UI/shop_item.gd
+++ UI/shop_item.gd
@@ -1,12 +1,12 @@
 extends Panel
-const BUILDING_PLACEMENT = preload("uid://fjg6m8jxharl")
+const BUILDING_PLACEMENT: Resource = preload("uid://fjg6m8jxharl")
 @onready var item_icon: TextureRect = $ItemIcon
-const BUFFET = preload("uid://dlayonr1hr4tv")
+const BUFFET: Resource = preload("uid://dlayonr1hr4tv")
 
 
 var building_preview: Node2D
-var click_holding = false
-var can_place = false
+var click_holding: bool = false
+var can_place: bool = false
 @export var item_price: float = 0.0
 @export var building: Node2D
 var building_path: PackedScene
@@ -42,10 +42,10 @@ func _gui_input(event: InputEvent) -> void:
 				
 				building = building_path.instantiate()
 				
-				var mouse_pos = get_global_mouse_position()
-				var tile_map = get_tree().current_scene.find_child("BuildingPlacement")
-				var tile_coords = tile_map.local_to_map(tile_map.to_local(mouse_pos))
-				var tile_global_pos = tile_map.to_global(tile_map.map_to_local(tile_coords))
+				var mouse_pos: Vector2 = get_global_mouse_position()
+				var tile_map: TileMapLayer = get_tree().current_scene.find_child("BuildingPlacement")
+				var tile_coords: Vector2 = tile_map.local_to_map(tile_map.to_local(mouse_pos))
+				var tile_global_pos: Vector2 = tile_map.to_global(tile_map.map_to_local(tile_coords))
 				
 				building.global_position = tile_global_pos
 				
@@ -80,12 +80,12 @@ func can_place_building(place: bool) -> void:
 	can_place = place
 
 # check if lmb is held down, and if the building preview exists
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if click_holding and is_instance_valid(building_preview) and can_place:
-			var mouse_pos = get_global_mouse_position()
-			var tile_map = get_tree().current_scene.find_child("BuildingPlacement")
-			var tile_coords = tile_map.local_to_map(tile_map.to_local(mouse_pos))
-			var tile_global_pos = tile_map.to_global(tile_map.map_to_local(tile_coords))
+			var mouse_pos: Vector2 = get_global_mouse_position()
+			var tile_map: TileMapLayer = get_tree().current_scene.find_child("BuildingPlacement")
+			var tile_coords: Vector2 = tile_map.local_to_map(tile_map.to_local(mouse_pos))
+			var tile_global_pos: Vector2 = tile_map.to_global(tile_map.map_to_local(tile_coords))
 			
 			building_preview.global_position = tile_global_pos
 	elif click_holding and is_instance_valid(building_preview):
