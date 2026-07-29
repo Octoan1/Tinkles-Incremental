ff1ddc5ce54f0ea62e4ccc704219903bc8e23ae3
Octoan1 <okloepping@gmail.com>
2026-07-29 18:34:22 -0500

    added lemming debug spawn key
    press [1] and lemming will spawn on mouse pos

--- Game Logic/lemming_spawner.gd
+++ Game Logic/lemming_spawner.gd
@@ -3,7 +3,17 @@ extends Node2D
 @export var scene: PackedScene
 
 func _on_timer_timeout() -> void:
-	var lemming: Node2D = scene.instantiate()
-	
-	
+	create_lemming()
+
+func create_lemming(pos: Vector2 = Vector2.ZERO) -> void:
+	var lemming: Lemming = scene.instantiate()
 	self.add_child(lemming)
+	if pos:
+		lemming.global_position = pos
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		var event_key: InputEventKey = event
+		if event_key.pressed and event_key.keycode == Key.KEY_1:
+			create_lemming(get_global_mouse_position())
+			
